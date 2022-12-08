81ac9ca36f83c1797feb9ae5bb416495c24311b4
Haweanah <ahauwa48@yahoo.com>
2022-12-08 04:08:35 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 /**
  * main - Entry
  * 
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
-	return (0);
+		return (0);
 }	
