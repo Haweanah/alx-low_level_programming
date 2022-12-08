bfb3d89841eb0e56b3c0943d7ac0aa757b742c5e
Haweanah <ahauwa48@yahoo.com>
2022-12-08 04:57:04 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+
+
 /**
- * main - Entry
- * Return: Always 0 
+ * main - Entry Point
+ *
+ *
+ * Return: Always 0(Success) 
  */
+
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");	
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
+
+
 }
