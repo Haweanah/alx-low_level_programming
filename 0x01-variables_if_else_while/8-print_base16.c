22d3615af37b9c4cc248038c348cbe68896a9ac6
Haweanah <ahauwa48@yahoo.com>
2022-12-09 15:17:14 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,7 +4,7 @@
  * main - main block
  * Description: prints all single digit number of base 10
  * starting from 0, followed by a new line
- * Return: Always 0
+ * Return: Always 0 (success)
  */
 int main(void)
 {
