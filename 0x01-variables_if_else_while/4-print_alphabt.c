d99096de62f8025a580596734ae86cbaef741048
Haweanah <ahauwa48@yahoo.com>
2022-12-09 13:16:53 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,9 +2,8 @@
 
 
 /**
- * main - prints the alphabet in lower cas
- * followed by a new line except q and e
- * Return: Always 0
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
