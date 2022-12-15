74c204822a4fbe261174664e5800bbe35a87bce1
Haweanah <ahauwa48@yahoo.com>
2022-12-15 03:06:17 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,6 @@
 
 void print_times_table(int n)
 {
-	
 	int a = 0, rep, b;
 
 	if (n < 0 || n > 15)
@@ -18,17 +17,16 @@ void print_times_table(int n)
 	{
 		for (b = 0; b <= n; b++)
 		{
-			rep = a *b;
+			rep = a * b;
 			if (b == 0)
 				_putchar('0' + rep);
 			if (rep < 10)
 			{
 				_putchar(' ');
-				_putchar(' ');	
-				_putchar('0' +rep);
+				_putchar(' ');
+				_putchar('0' + rep);
 			}
-		}
-		if (rep < 100)
+			else if (rep < 100)
 		{
 			_putchar(' ');
 			_putchar('0' + rep / 10);
@@ -44,7 +42,8 @@ void print_times_table(int n)
 		{
 			_putchar(',');
 			_putchar(' ');
-	}
+		}
+		}
 	_putchar('\n');
 	a++;
 	}
