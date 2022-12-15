984d032a0a866911cce4f12c8b6b40dfea91e8b7
Haweanah <ahauwa48@yahoo.com>
2022-12-15 03:24:19 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,61 +1,18 @@
 #include <stdio.h>
 
 /**
- * numlength - returns the length of string
- * @num: operand number
- * Return: number of digits
- */
-int numlength(int num)
-{
-	int length = 0;
-
-	if (!num)
-	{
-		return (1);
-	}
-
-	while (num)
-	{
-		num = num / 10;
-		length += 1;
-	}
-
-	return (length);
-}
-/**
- * main - prints the first 98 fibonaci sequences
- * Return: 0
+ * main - main block
+ * Description: computes and prints the sum of all
+ * the multiples of 3 or 5 below 1024 (excluded), followed by a new line
+ * Return: Always 0
  */
 int main(void)
 {
-	unsigned long f1 = 1, f2 = 2, tmp,
- mx = 100000000, f1o = 0, f2o = 0, tmpo = 0;
-	short int i = 1, initial0s;
-
-	while (i <= 98)
-	{
-		if (f1o > 0)
-			printf("%lu", f1o);
-		initial0s = numlength(mx) - 1 - numlength(f1);
-		while (f1o > 0 && initial0s > 0)
-		{
-			printf("%i", 0);
-			initial0s--;
-		}
-		printf("%lu", f1);
-
-		tmp = (f1 + f2) % mx;
-		tmpo = f1o + f2o + (f1 + f2) / mx;
-		f1 = f2;
-		f1o = f2o;
-		f2 = tmp;
-		f2o = tmpo;
-
-		if (i != 98)
-			printf(",");
-		else
-			printf("\n");
-		i++;
-	}
-	return (0);
-}
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int 1 = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsig
