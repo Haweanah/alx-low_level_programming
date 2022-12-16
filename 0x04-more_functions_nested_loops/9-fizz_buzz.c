31ae5f0cb4cb8cefde212de0f930d91c540de8cd
Haweanah <ahauwa48@yahoo.com>
2022-12-16 05:32:24 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,32 +7,36 @@
 int main(void)
 {
 int p = 100;
-n = 1;
+int i;
 
 i = 1;
 while (i <= p)
 {
-if ((i % 3 == 0) && (i % 5 == 0))
+if (i % 3 == 0 && i % 5 == 0)
 {
-printf("FizzBuzz");
+printf("FizzBuzz ");
 }
 
 else if (i % 3 == 0)
 {
-printf("Fizz);
+printf("Fizz ");
 }
 else if (i % 5 == 0)
 {
 if (i < p)
+printf("Buzz ");
+
+else
 printf("Buzz");
 }
 
 else
 {
-printf("%i, i);
+printf("%i ", i);
 }
 
 i++;
+
 }
 printf('\n');
 return (0);
