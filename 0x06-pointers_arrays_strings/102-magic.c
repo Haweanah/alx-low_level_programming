95192807718d48c5349bdb622556176f55989620
Haweanah <ahauwa48@yahoo.com>
2023-01-03 09:27:22 -0800

    - 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,10 +3,10 @@
 int main(void)
 {
 	int n;
-	int a[5];
+	int g[5];
 	int *p;
 
-	a[2] = 1024;
+	g[2] = 1024;
 	p = &n;
 /*
  * write your line of code here...
@@ -19,6 +19,6 @@ int main(void)
  */
 	*(p + 5) = 98;
 /* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
+	printf("g[2] = %d\n", g[2]);
 	return (0);
 }
