235d6625a089aa3fc9e4eb2cf4489e4ec36e6cea
Haweanah <ahauwa48@yahoo.com>
2023-01-05 09:44:35 -0800

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,7 +7,7 @@ int actual_prime(int n, int i);
  * @n: number to evaluate
  * Return: 1 if n is a prime number, 0 if not
  */
-int is_prime_number(int n);
+int is_prime_number(int n)
 {
 if (n <= 1)
 return (0);
@@ -20,7 +20,7 @@ return (actual_prime(n, n - 1));
  * @i: iterator
  * Return: 1 if n is prime, 0 if not
  */
-int actual_prime(int n, int i);
+int actual_prime(int n, int i)
 {
 if (i == 1)
 return (1);
