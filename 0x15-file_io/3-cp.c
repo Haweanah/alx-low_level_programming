96f733b7ab43316b53d941bc46146de10fb4d2ab
Haweanah <ahauwa48@yahoo.com>
2023-02-06 16:04:53 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -69,14 +69,13 @@ from = open(argv[1], O_RDONLY);
 r = read(from, buffer, 1024);
 
 to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-do
-{
+do {
 if (from == -1 || r == -1)
 {
 dprintf(STDERR_FILENO,
 "Error: Can't read from file %s\n", argv[1]);
 free(buffer);
-exit(98);				
+exit(98);
 }
 w = write(to, buffer, r);
 
@@ -87,8 +86,7 @@ free(buffer);
 exit(99);
 r = read(from, buffer, 1024);
 to = open(argv[2], O_WRONLY | O_APPEND);
-}
-while (r > 0);
+} while (r > 0);
 free(buffer);
 close_file(from);
 close_file(to);
