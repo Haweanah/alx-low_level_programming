0406798b6991761471dcd5f7e3f82f7789880cb1
Haweanah <ahauwa48@yahoo.com>
2023-02-06 16:07:50 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -72,8 +72,7 @@ to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 do {
 if (from == -1 || r == -1)
 {
-dprintf(STDERR_FILENO,
-"Error: Can't read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 free(buffer);
 exit(98);
 }
